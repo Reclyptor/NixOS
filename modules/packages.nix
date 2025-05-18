6a55b31e5581e31c2ad783c904facf70bfd8f799
Reclyptor <5952751+Reclyptor@users.noreply.github.com>
2025-05-18 00:56:31 -0500

    Added JetBrains IDEs

--- modules/packages.nix
+++ modules/packages.nix
@@ -22,6 +22,15 @@
     hyprpicker
     hyprsunset
     imv
+    jetbrains.clion
+    jetbrains.datagrip
+    jetbrains.goland
+    jetbrains.idea-ultimate
+    jetbrains.phpstorm
+    jetbrains.pycharm-professional
+    jetbrains.ruby-mine
+    jetbrains.rust-rover
+    jetbrains.webstorm
     kitty
     kitty-themes
     krita
