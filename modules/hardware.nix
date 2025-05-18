15466f17c61e1b328867409956b13f92f1807773
Reclyptor <5952751+Reclyptor@users.noreply.github.com>
2025-05-18 03:39:45 -0500

    Updated OpenGL Entries

--- modules/hardware.nix
+++ modules/hardware.nix
@@ -2,25 +2,26 @@
   services.xserver.videoDrivers = [ "nvidia" ];
 
   hardware = {
-    graphics.enable = true;
-    graphics.enable32Bit = true;
     nvidia = {
       modesetting.enable = true;
       nvidiaSettings = true;
       open = true;
       package = config.boot.kernelPackages.nvidiaPackages.stable;
     };
-    opengl = {
+    graphics = {
       enable = true;
+      enable32Bit = true;
       extraPackages = with pkgs; [
-	vulkan-loader
+	libGL
+        libglvnd
+        vulkan-loader
         vulkan-tools
         vulkan-validation-layers
       ];
       extraPackages32 = with pkgs; [
+	libGL
+        libglvnd
         vulkan-loader
-        vulkan-tools
-        vulkan-validation-layers
       ];
     };
   };
