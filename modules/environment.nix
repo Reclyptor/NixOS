3c1fdefbbc91f71ff460abb8fe2827e1bd19434e
Reclyptor <5952751+Reclyptor@users.noreply.github.com>
2025-05-18 06:06:26 -0500

    Updated Environment Variables

--- modules/environment.nix
+++ modules/environment.nix
@@ -1,17 +1,22 @@
 { config, pkgs, ... }: {
   environment.variables = {
-    EDITOR = "nvim";
-    GTK_THEME = "Adwaita-dark";
-    QT_STYLE_OVERRIDE = "Adwaita-dark";
   };
 
   environment.sessionVariables = {
-    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    EDITOR = "nvim";
+    DEFAULT_BROWSER = "${pkgs.brave}/bin/brave";
     GBM_BACKEND = "nvidia-drm";
+    GTK_THEME = "Adwaita-dark";
     LIBVA_DRIVER_NAME = "nvidia";
     NIXOS_OZONE_WL = "1";
+    QT_STYLE_OVERRIDE = "Adwaita-dark";
+    WINE_FULLSCREEN_FSR= "1";
+    WINE_FULLSCREEN_FSR_STRENGTH= "2";
+    WINE_VIRTUAL_DESKTOP= "0";
     WLR_NO_HARDWARE_CURSORS = "1";
     XDG_SESSION_TYPE = "wayland";
-    DEFAULT_BROWSER = "${pkgs.brave}/bin/brave";
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    __GL_GSYNC_ALLOWED = "0";
+    __GL_VRR_ALLOWED = "0";
   };
 }
