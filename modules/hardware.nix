9a749a3ae4589ac0decabf4fa885757f55edd598
Reclyptor <5952751+Reclyptor@users.noreply.github.com>
2025-05-18 03:27:31 -0500

    Added OpenGL Entries

--- modules/hardware.nix
+++ modules/hardware.nix
@@ -3,11 +3,25 @@
 
   hardware = {
     graphics.enable = true;
+    graphics.enable32Bit = true;
     nvidia = {
       modesetting.enable = true;
       nvidiaSettings = true;
       open = true;
       package = config.boot.kernelPackages.nvidiaPackages.stable;
+    };
+    opengl = {
+      enable = true;
+      extraPackages = with pkgs; [
+	vulkan-loader
+        vulkan-tools
+        vulkan-validation-layers
+      ];
+      extraPackages32 = with pkgs; [
+        vulkan-loader
+        vulkan-tools
+        vulkan-validation-layers
+      ];
     };
   };
 }
