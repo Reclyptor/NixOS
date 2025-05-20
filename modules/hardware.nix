9139285376ace0416924fdb37f66d06ca1c36d2b
Reclyptor <5952751+Reclyptor@users.noreply.github.com>
2025-05-20 00:12:58 -0500

    Updated Nvidia Module

--- modules/hardware.nix
+++ modules/hardware.nix
@@ -1,36 +1,20 @@
 { config, pkgs, ... }: {
   services.xserver.videoDrivers = [ "nvidia" ];
-
   hardware = {
-    nvidia = {
-      open = true;
-      nvidiaSettings = true;
-      package = config.boot.kernelPackages.nvidiaPackages.stable;
-      modesetting.enable = true;
-      powerManagement.enable = false;
-    };
-
     graphics = {
       enable = true;
       enable32Bit = true;
-      extraPackages = with pkgs; [
-        vulkan-loader
-        vulkan-validation-layers
-	vulkan-extension-layer
-        vulkan-tools
-	libvdpau
-        vaapiVdpau
-        libglvnd
-      ];
-      extraPackages32 = with pkgs; [
-        vulkan-loader
-        vulkan-validation-layers
-	vulkan-extension-layer
-        vulkan-tools
-	libvdpau
-        vaapiVdpau
-        libglvnd
-      ];
+    };
+
+    nvidia = {
+      modesetting.enable = true;
+      powerManagement = {
+        enable = false;
+        finegrained = false;
+      };
+      open = false;
+      nvidiaSettings = true;
+      package = config.boot.kernelPackages.nvidiaPackages.beta;
     };
 
     bluetooth = {
