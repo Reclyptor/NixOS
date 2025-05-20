0d32f30087ed67f2788f92dd9ea94c30d463076e
Reclyptor <5952751+Reclyptor@users.noreply.github.com>
2025-05-19 20:45:27 -0500

    Pinned Gamescope Version to 3.16.2

--- modules/packages/gaming.nix
+++ modules/packages/gaming.nix
@@ -1,10 +1,19 @@
-{ pkgs, ... }: {
+{ config, pkgs, ... }: {
   nixpkgs.config.allowUnfree = true;
 
-  environment.systemPackages = with pkgs; [
-    steam
-    mangohud
-    gamescope
-    protonup-qt
-  ];
+  environment.systemPackages =
+    let
+      pinnedPkgs = import (builtins.fetchTarball {
+        url = "https://github.com/NixOS/nixpkgs/archive/1750f3c1c89488e2ffdd47cab9d05454dddfb734.tar.gz";
+        sha256 = "1nrwlaxd0f875r2g6v9brrwmxanra8pga5ppvawv40hcalmlccm0";
+      }) {
+        system = pkgs.system; # ← THIS IS THE FIX
+      };
+    in
+    with pkgs; [
+      steam
+      mangohud
+      pinnedPkgs.gamescope
+      protonup-qt
+    ];
 }
