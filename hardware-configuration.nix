9a18cc3899c0a252ac576dd6da60b055c42bd802
Reclyptor <5952751+Reclyptor@users.noreply.github.com>
2025-05-20 19:23:05 -0500

    Fixed sg and xpad Kernel Module Declaration

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -8,9 +8,9 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" "sr_mod" "sg" "xpad" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" "sr_mod" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" ];
+  boot.kernelModules = [ "kvm-intel" "sg" "xpad" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
