5123e664a4051402f2e457b45c0aca2debd0dda4
Reclyptor <5952751+Reclyptor@users.noreply.github.com>
2025-05-23 17:22:08 -0500

    Moved Experimental Flags to Configuration Module

--- configuration.nix
+++ configuration.nix
@@ -1,6 +1,7 @@
 { config, pkgs, lib, ... }: (
   let moduleDirectory = ./modules; in {
     system.stateVersion = "24.11";
+    nix.settings.experimental-features = [ "nix-command" "flakes" ];
     imports = builtins.attrValues (lib.genAttrs (builtins.attrNames (builtins.readDir moduleDirectory)) (name: import (moduleDirectory + "/${name}")));
   }
 )
