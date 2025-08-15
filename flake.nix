eb043db641524df735a3d920555240906b8bf05c
Reclyptor <5952751+Reclyptor@users.noreply.github.com>
2025-08-15 03:24:46 -0500

    Modularized Flake Configurations

--- flake.nix
+++ flake.nix
@@ -7,8 +7,8 @@
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
-        ./configuration.nix
-        ./hardware-configuration.nix
+        ./hosts/nixos/configuration.nix
+        ./hosts/nixos/hardware-configuration.nix
       ];
     };
   };
--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -1,5 +1,5 @@
 { config, pkgs, lib, ... }: (
-  let moduleDirectory = ./modules; in {
+  let moduleDirectory = ../../modules; in {
     system.stateVersion = "24.11";
     nix.settings.experimental-features = [ "nix-command" "flakes" ];
     imports = builtins.attrValues (lib.genAttrs (builtins.attrNames (builtins.readDir moduleDirectory)) (name: import (moduleDirectory + "/${name}")));
