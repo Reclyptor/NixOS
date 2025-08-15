8cb99f582744295391afd4af86a0262642ea15fe
Reclyptor <5952751+Reclyptor@users.noreply.github.com>
2025-08-15 18:31:16 -0500

    Updated tuigreet

--- modules/login.nix
+++ modules/login.nix
@@ -3,7 +3,7 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --cmd ${pkgs.hyprland}/bin/Hyprland";
+        command = "${pkgs.tuigreet}/bin/tuigreet --cmd ${pkgs.hyprland}/bin/Hyprland";
         user = "reclyptor";
       };
     };
