5372cc3179a90e761c3d4df1a6407287d5beeb39
Reclyptor <5952751+Reclyptor@users.noreply.github.com>
2026-02-06 22:05:43 -0600

    Updated yt-dlp

--- hosts/nixos/overlays/yt-dlp.nix
+++ hosts/nixos/overlays/yt-dlp.nix
@@ -2,12 +2,12 @@
   nixpkgs.overlays = [
     (final: prev: {
       yt-dlp = prev.yt-dlp.overrideAttrs (oldAttrs: rec {
-        version = "2026.01.31";
+        version = "2026.02.04";
         src = prev.fetchFromGitHub {
           owner = "yt-dlp";
           repo = "yt-dlp";
           tag = version;
-          hash = "sha256-3sXXyWuQI6KTOQIkkOfJhCTBBh3Zkv59ENhkrz9Sgxc=";
+          hash = "sha256-KXnz/ocHBftenDUkCiFoBRBxi6yWt0fNuRX+vKFWDQw=";
         };
         postPatch = builtins.replaceStrings
           [ "< (0, 14)" ]
