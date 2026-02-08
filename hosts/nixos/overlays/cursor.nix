a07daf52967fdb849e916c91af44a18f09a32ba4
Reclyptor <5952751+Reclyptor@users.noreply.github.com>
2026-02-08 03:46:26 -0600

    Updated Cursor

--- hosts/nixos/overlays/cursor.nix
+++ hosts/nixos/overlays/cursor.nix
@@ -8,14 +8,14 @@
           # Define new sources with updated version
           sources = {
             x86_64-linux = prev.fetchurl {
-              url = "https://downloads.cursor.com/production/2ca326e0d1ce10956aea33d54c0e2d8c13c58a32/linux/x64/Cursor-2.3.41-x86_64.AppImage";
-              hash = "sha256-ItUgknMzSDeXxN3Yi/pz2wZoz7vVVqx9nGXuGmbHbXc=";
+              url = "https://downloads.cursor.com/production/f3f5cec40024283013878b50c4f9be4002e0b587/linux/x64/Cursor-2.4.28-x86_64.AppImage";
+              hash = "sha256-Vkat7mTge3yDDzWAfwuapuwsUCPBrl41THA1WEAvTyY=";
             };
           };
           
           source = sources.${hostPlatform.system};
           pname = "cursor";
-          version = "2.3.41";
+          version = "2.4.28";
         in {
           # Override version and src with proper AppImage extraction
           inherit version;
