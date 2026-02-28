b84d4a793230ede6d6279686fb6934ec9c3eb0c9
Reclyptor <5952751+Reclyptor@users.noreply.github.com>
2026-02-27 20:30:16 -0600

    Removed Steam Pinned Configuration

--- hosts/nixos/modules/steam.nix
+++ hosts/nixos/modules/steam.nix
@@ -1,26 +1,14 @@
 { config, pkgs, ... }: {
   environment.sessionVariables = {
     STEAM_RUNTIME = "1";
-    ENABLE_GAMESCOPE_WSI = "1";
   };
 
-  environment.systemPackages =
-    let
-      pinnedPkgs = import (builtins.fetchTarball {
-        url = "https://github.com/NixOS/nixpkgs/archive/1750f3c1c89488e2ffdd47cab9d05454dddfb734.tar.gz";
-        sha256 = "1nrwlaxd0f875r2g6v9brrwmxanra8pga5ppvawv40hcalmlccm0";
-      }) {
-        system = pkgs.stdenv.hostPlatform.system;
-      };
-    in
-    with pkgs; [
-      steam
-      gamemode
-      mangohud
-      pinnedPkgs.gamescope
-      protonup-qt
-    ]
-  ;
+  environment.systemPackages = with pkgs; [
+    steam
+    gamemode
+    mangohud
+    protonup-qt
+  ];
 
   programs.steam = {
     enable = true;
