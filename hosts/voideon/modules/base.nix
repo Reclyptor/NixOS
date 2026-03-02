50219d864cf372aa0c4bd8f8100c34256e7f0af7
Reclyptor <5952751+Reclyptor@users.noreply.github.com>
2026-03-02 01:01:40 -0600

    Updated k3s Hardware Configurations

--- hosts/voideon/modules/base.nix
+++ hosts/voideon/modules/base.nix
@@ -13,7 +13,7 @@
   boot.kernelParams = [ "amdgpu.dc=1" "amdgpu.dpm=1" ];
   boot.supportedFilesystems = [ "zfs" "nfs" ];
 
-  networking.hostId = "00000003";
+  networking.hostId = "1364e987";
   networking.networkmanager.enable = true;
 
   time.timeZone = "America/Chicago";
@@ -35,5 +35,7 @@
   services.xserver.xkb = { layout = "us"; variant = ""; };
 
   nixpkgs.config.allowUnfree = true;
+
+  environment.systemPackages = [ pkgs.kitty.terminfo ];
 }
 
