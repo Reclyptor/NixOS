7229e43b30de9cbcca83b3591e6efe9a80922a2c
Reclyptor <5952751+Reclyptor@users.noreply.github.com>
2026-03-02 12:38:45 -0600

    Updated k3s Node Nvidia Configuration

--- hosts/bytheon/modules/nvidia.nix
+++ hosts/bytheon/modules/nvidia.nix
@@ -1,5 +1,8 @@
-{ config, pkgs, ... }:
-let driver = config.boot.kernelPackages.nvidiaPackages.stable; in {
+{ config, lib, pkgs, ... }:
+let
+  driver = config.boot.kernelPackages.nvidiaPackages.stable;
+  nctk = config.hardware.nvidia-container-toolkit.package;
+in {
   nixpkgs.config.nvidia.acceptLicense = true;
 
   services.xserver.videoDrivers = [ "nvidia" ];
@@ -7,7 +10,7 @@ let driver = config.boot.kernelPackages.nvidiaPackages.stable; in {
   environment.sessionVariables = {
     CUDA_PATH = "${pkgs.cudaPackages.cudatoolkit}";
     CUDA_HOME = "${pkgs.cudaPackages.cudatoolkit}";
-    LD_LIBRARY_PATH = "$LD_LIBRARY_PATH:${pkgs.linuxPackages.nvidia_x11}/lib:${pkgs.cudaPackages.cudatoolkit}/lib:${pkgs.cudaPackages.cudnn}/lib";
+    LD_LIBRARY_PATH = "$LD_LIBRARY_PATH:${driver}/lib:${pkgs.cudaPackages.cudatoolkit}/lib:${pkgs.cudaPackages.cudnn}/lib";
     EXTRA_LDFLAGS = "-L${pkgs.cudaPackages.cudatoolkit}/lib -L${pkgs.cudaPackages.cudnn}/lib";
     EXTRA_CCFLAGS = "-I${pkgs.cudaPackages.cudatoolkit}/include -I${pkgs.cudaPackages.cudnn}/include";
   };
@@ -15,13 +18,33 @@ let driver = config.boot.kernelPackages.nvidiaPackages.stable; in {
   environment.systemPackages = with pkgs; [
     cudaPackages.cudatoolkit
     cudaPackages.cudnn
+    nvidia-container-toolkit
+    nvidia-container-toolkit.tools
   ];
+
+  environment.etc."nvidia-container-runtime/config.toml".text = ''
+    disable-require = true
+    supported-driver-capabilities = "compat32,compute,display,graphics,ngx,utility,video"
+    [nvidia-container-cli]
+    environment = []
+    ldconfig = "@${lib.getExe' pkgs.glibc "ldconfig"}"
+    load-kmods = true
+    no-cgroups = false
+    path = "${lib.getExe' pkgs.libnvidia-container "nvidia-container-cli"}"
+    [nvidia-container-runtime]
+    mode = "cdi"
+    runtimes = ["runc", "crun"]
+    [nvidia-container-runtime-hook]
+    path = "${lib.getOutput "tools" nctk}/bin/nvidia-container-runtime-hook"
+    skip-mode-detection = false
+    [nvidia-ctk]
+    path = "${lib.getExe' nctk "nvidia-ctk"}"
+  '';
 
   hardware = {
     graphics = {
       enable = true;
       enable32Bit = true;
-      package = driver;
     };
 
     nvidia = {
@@ -29,12 +52,11 @@ let driver = config.boot.kernelPackages.nvidiaPackages.stable; in {
       nvidiaSettings = false;
       modesetting.enable = true;
       package = driver;
-      powerManagement = {
-        enable = false;
-        finegrained = false;
-      };
     };
 
-    nvidia-container-toolkit.enable = true;
+    nvidia-container-toolkit = {
+      enable = true;
+      device-name-strategy = "uuid";
+    };
   };
 }
