37e878ee330d9ab8468c75a084bd79750a2922bd
Reclyptor <5952751+Reclyptor@users.noreply.github.com>
2026-03-14 00:54:12 -0500

    Updated Secrets

--- home/bash/00-init.nix
+++ home/bash/00-init.nix
@@ -1,17 +1,31 @@
 { config, pkgs, ... }: {
-  programs.bash.sessionVariables = {
-    # Google Cloud SQL 
-    GCP_MYSQL_CA_CERT = "/etc/ssl/cloudsql/server-ca.pem";
-    GCP_MYSQL_CLIENT_CERT = "/etc/ssl/cloudsql/client-cert.pem";
-    GCP_MYSQL_CLIENT_KEY = "/etc/ssl/cloudsql/client-key.pem";
-  };
-  
   programs.bash.initExtra = ''
     # PATH additions
     export PATH=$PATH:~/.local/bin/:~/.local/share/JetBrains/Toolbox/scripts
-    
+
     if [ -f ~/.config/sops/secrets/bash/gcp-mysql-host ]; then
       export GCP_MYSQL_HOST=$(cat ~/.config/sops/secrets/bash/gcp-mysql-host)
+    fi
+
+    if [ -f ~/.config/sops/secrets/bash/k3s-host ]; then
+      export K3S_HOST=$(cat ~/.config/sops/secrets/bash/k3s-host)
+    fi
+    if [ -f ~/.config/sops/secrets/bash/k3s-mysql-password ]; then
+      export K3S_MYSQL_PASSWORD=$(cat ~/.config/sops/secrets/bash/k3s-mysql-password)
+    fi
+    if [ -f ~/.config/sops/secrets/bash/k3s-mongodb-password ]; then
+      export K3S_MONGODB_PASSWORD=$(cat ~/.config/sops/secrets/bash/k3s-mongodb-password)
+    fi
+
+    # Google Cloud SQL certificates (managed by sops-nix)
+    if [ -f ~/.config/sops/secrets/bash/gcp-mysql-ca-cert ]; then
+      export GCP_MYSQL_CA_CERT=~/.config/sops/secrets/bash/gcp-mysql-ca-cert
+    fi
+    if [ -f ~/.config/sops/secrets/bash/gcp-mysql-client-cert ]; then
+      export GCP_MYSQL_CLIENT_CERT=~/.config/sops/secrets/bash/gcp-mysql-client-cert
+    fi
+    if [ -f ~/.config/sops/secrets/bash/gcp-mysql-client-key ]; then
+      export GCP_MYSQL_CLIENT_KEY=~/.config/sops/secrets/bash/gcp-mysql-client-key
     fi
 
     if [ -f ~/.config/sops/secrets/bash/atlas-mongodb-host ]; then
--- home/bash/10-aliases.nix
+++ home/bash/10-aliases.nix
@@ -3,7 +3,9 @@
     ls = "eza -a --icons=always";
     tree = "eza -a --tree --icons=always";
     
-    mysql-reclyptor = "mysql -ureclyptor -p -h \${GCP_MYSQL_HOST} --ssl-ca=\${GCP_MYSQL_CA_CERT} --ssl-cert=\${GCP_MYSQL_CLIENT_CERT} --ssl-key=\${GCP_MYSQL_CLIENT_KEY}";
-    mongo-reclyptor = "mongosh '\${ATLAS_MONGODB_HOST}' --apiVersion 1 --username reclyptor";
+    mysql-reclyptor = "mysql -u$USER -p -h \${GCP_MYSQL_HOST} --ssl-ca=\${GCP_MYSQL_CA_CERT} --ssl-cert=\${GCP_MYSQL_CLIENT_CERT} --ssl-key=\${GCP_MYSQL_CLIENT_KEY}";
+    mysql-k3s = "mysql -u$USER -p\${K3S_MYSQL_PASSWORD} -h \${K3S_HOST}";
+    mongo-reclyptor = "mongosh '\${ATLAS_MONGODB_HOST}' --apiVersion 1 --username $USER";
+    mongo-k3s = "mongosh --host \${K3S_HOST} --username $USER --password \${K3S_MONGODB_PASSWORD}";
   };
 }
