24f5abaec76bcf510b7516c1a751e9eff24bc63c
Reclyptor <5952751+Reclyptor@users.noreply.github.com>
2026-03-14 00:54:12 -0500

    Updated Secrets

--- home/secrets.nix
+++ home/secrets.nix
@@ -11,6 +11,24 @@
       "bash/gcp-mysql-host" = {
         path = "${config.home.homeDirectory}/.config/sops/secrets/bash/gcp-mysql-host";
       };
+      "bash/gcp-mysql-ca-cert" = {
+        path = "${config.home.homeDirectory}/.config/sops/secrets/bash/gcp-mysql-ca-cert";
+      };
+      "bash/gcp-mysql-client-cert" = {
+        path = "${config.home.homeDirectory}/.config/sops/secrets/bash/gcp-mysql-client-cert";
+      };
+      "bash/gcp-mysql-client-key" = {
+        path = "${config.home.homeDirectory}/.config/sops/secrets/bash/gcp-mysql-client-key";
+      };
+      "bash/k3s-host" = {
+        path = "${config.home.homeDirectory}/.config/sops/secrets/bash/k3s-host";
+      };
+      "bash/k3s-mysql-password" = {
+        path = "${config.home.homeDirectory}/.config/sops/secrets/bash/k3s-mysql-password";
+      };
+      "bash/k3s-mongodb-password" = {
+        path = "${config.home.homeDirectory}/.config/sops/secrets/bash/k3s-mongodb-password";
+      };
       "bash/atlas-mongodb-host" = {
         path = "${config.home.homeDirectory}/.config/sops/secrets/bash/atlas-mongodb-host";
       };
