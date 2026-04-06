8d0fe2536ee843f730771dd38389e1d77fcaf35f
Reclyptor <5952751+Reclyptor@users.noreply.github.com>
2026-04-05 19:03:56 -0500

    Rolled Back Nvidia Driver

--- hosts/nixos/modules/nvidia.nix
+++ hosts/nixos/modules/nvidia.nix
@@ -23,7 +23,14 @@
       open = true; # Required for Blackwell (RTX 50xx)
       modesetting.enable = true;
       nvidiaSettings = true;
-      package = config.boot.kernelPackages.nvidiaPackages.stable;
+      package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+        version = "590.48.01";
+        sha256_64bit = "sha256-ueL4BpN4FDHMh/TNKRCeEz3Oy1ClDWto1LO/LWlr1ok=";
+        sha256_aarch64 = "sha256-FOz7f6pW1NGM2f75kbP6LbNijxKj5ZtZ08bm0aC+/YA=";
+        openSha256 = "sha256-hECHfguzwduEfPo5pCDjWE/MjtRDhINVr4b1awFdP44=";
+        settingsSha256 = "sha256-NWsqUciPa4f1ZX6f0By3yScz3pqKJV1ei9GvOF8qIEE=";
+        persistencedSha256 = "sha256-wsNeuw7IaY6Qc/i/AzT/4N82lPjkwfrhxidKWUtcwW8=";
+      };
       powerManagement.enable = false;
       powerManagement.finegrained = false;
     };
