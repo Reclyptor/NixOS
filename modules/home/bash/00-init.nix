5160919efe8bb7f6254e181c917bdea70a34ed3b
Reclyptor <5952751+Reclyptor@users.noreply.github.com>
2026-07-03 06:22:14 -0500

    Generated Bash Secret Exports From A Table

--- modules/home/bash/00-init.nix
+++ modules/home/bash/00-init.nix
@@ -1,41 +1,39 @@
 { ... }: {
-  flake.modules.homeManager.base = { config, pkgs, ... }: {
-    programs.bash.initExtra = ''
-      # PATH additions
-      export PATH=$PATH:~/.local/bin/:~/.local/share/JetBrains/Toolbox/scripts
+  flake.modules.homeManager.base = { lib, ... }:
+    let
+      secretsDir = "~/.config/sops/secrets/bash";
 
-      if [ -f ~/.config/sops/secrets/bash/gcp-mysql-host ]; then
-        export GCP_MYSQL_HOST=$(cat ~/.config/sops/secrets/bash/gcp-mysql-host)
-      fi
+      # Secrets whose decrypted CONTENT becomes the variable value.
+      valueExports = {
+        GCP_MYSQL_HOST = "gcp-mysql-host";
+        K3S_HOST = "k3s-host";
+        K3S_MYSQL_PASSWORD = "k3s-mysql-password";
+        K3S_MONGODB_PASSWORD = "k3s-mongodb-password";
+        K3S_REDIS_PASSWORD = "k3s-redis-password";
+        ATLAS_MONGODB_HOST = "atlas-mongodb-host";
+      };
 
-      if [ -f ~/.config/sops/secrets/bash/k3s-host ]; then
-        export K3S_HOST=$(cat ~/.config/sops/secrets/bash/k3s-host)
-      fi
-      if [ -f ~/.config/sops/secrets/bash/k3s-mysql-password ]; then
-        export K3S_MYSQL_PASSWORD=$(cat ~/.config/sops/secrets/bash/k3s-mysql-password)
-      fi
-      if [ -f ~/.config/sops/secrets/bash/k3s-mongodb-password ]; then
-        export K3S_MONGODB_PASSWORD=$(cat ~/.config/sops/secrets/bash/k3s-mongodb-password)
-      fi
+      # Secrets whose PATH becomes the variable value (certs read on use).
+      pathExports = {
+        GCP_MYSQL_CA_CERT = "gcp-mysql-ca-cert";
+        GCP_MYSQL_CLIENT_CERT = "gcp-mysql-client-cert";
+        GCP_MYSQL_CLIENT_KEY = "gcp-mysql-client-key";
+      };
 
-      # Google Cloud SQL certificates (managed by sops-nix)
-      if [ -f ~/.config/sops/secrets/bash/gcp-mysql-ca-cert ]; then
-        export GCP_MYSQL_CA_CERT=~/.config/sops/secrets/bash/gcp-mysql-ca-cert
-      fi
-      if [ -f ~/.config/sops/secrets/bash/gcp-mysql-client-cert ]; then
-        export GCP_MYSQL_CLIENT_CERT=~/.config/sops/secrets/bash/gcp-mysql-client-cert
-      fi
-      if [ -f ~/.config/sops/secrets/bash/gcp-mysql-client-key ]; then
-        export GCP_MYSQL_CLIENT_KEY=~/.config/sops/secrets/bash/gcp-mysql-client-key
-      fi
+      mkExports = toValue: lib.mapAttrsToList (name: file:
+        ''if [ -f ${secretsDir}/${file} ]; then export ${name}=${toValue "${secretsDir}/${file}"}; fi''
+      );
 
-      if [ -f ~/.config/sops/secrets/bash/k3s-redis-password ]; then
-        export K3S_REDIS_PASSWORD=$(cat ~/.config/sops/secrets/bash/k3s-redis-password)
-      fi
+      exports = lib.concatStringsSep "\n"
+        (mkExports (path: "$(cat ${path})") valueExports
+          ++ mkExports (path: path) pathExports);
+    in {
+      programs.bash.initExtra = ''
+        # PATH additions
+        export PATH=$PATH:~/.local/bin/:~/.local/share/JetBrains/Toolbox/scripts
 
-      if [ -f ~/.config/sops/secrets/bash/atlas-mongodb-host ]; then
-        export ATLAS_MONGODB_HOST=$(cat ~/.config/sops/secrets/bash/atlas-mongodb-host)
-      fi
-    '';
-  };
+        # Secrets decrypted by sops-nix, exported when present
+        ${exports}
+      '';
+    };
 }
