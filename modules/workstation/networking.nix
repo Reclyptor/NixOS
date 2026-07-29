df6facc3d350a0ae1122c62223735f1e168d8fb4
Reclyptor <5952751+Reclyptor@users.noreply.github.com>
2026-07-29 00:27:12 -0500

    Disabled WiFi Power Saving to Reduce Wireless Latency

--- modules/workstation/networking.nix
+++ modules/workstation/networking.nix
@@ -3,6 +3,7 @@
     networking.hostName = "nixos";
     networking.hostId = "bca3551f";
     networking.networkmanager.enable = true;
+    networking.networkmanager.wifi.powersave = false;
     networking.firewall.enable = true;
     environment.systemPackages = with pkgs; [
       networkmanagerapplet
