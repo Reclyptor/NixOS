7c935497343ea8e1b5c9fce1bd1fbf362e562a7b
Reclyptor <5952751+Reclyptor@users.noreply.github.com>
2026-08-02 04:14:25 -0500

    Updated yt-dlp Overlay Pin

--- modules/workstation/overlays/yt-dlp.nix
+++ modules/workstation/overlays/yt-dlp.nix
@@ -3,12 +3,12 @@
     nixpkgs.overlays = [
       (final: prev: {
         yt-dlp = prev.yt-dlp.overrideAttrs (oldAttrs: rec {
-          version = "2026.06.09";
+          version = "2026.07.04";
           src = prev.fetchFromGitHub {
             owner = "yt-dlp";
             repo = "yt-dlp";
             tag = version;
-            hash = "sha256-ykqTDPzKKIWRGSQmw2esCRKyYqDZKXRYDeba888tkDU=";
+            hash = "sha256-+oHcVylLXFJTRR6jXF6IXvgntXJz0tRdtnwTruRPkoc=";
           };
         });
       })
