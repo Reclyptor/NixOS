ae7443a9db6104cea25ee97dee419d1dad50c1a3
Reclyptor <5952751+Reclyptor@users.noreply.github.com>
2026-08-14 00:02:55 -0500

    Pointed the Host Modules at Their Fleet Inventory Entries

--- modules/hosts/fluxeon.nix
+++ modules/hosts/fluxeon.nix
@@ -1,15 +1,11 @@
-{ config, ... }: {
+{ config, ... }:
+{
   configurations.nixos.fluxeon = {
     imports = [
       ../../hardware/fluxeon.nix
       config.flake.modules.nixos.server
     ];
 
-    host = {
-      hostId = "491bbece";
-      wiredIp = "192.168.1.11";
-      wirelessIp = "192.168.1.21";
-      k3s.role = "server";
-    };
+    host = config.fleet.nodes.fluxeon;
   };
 }
