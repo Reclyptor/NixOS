5b7d37dbfb099fc25492d57d05520f13ec7fe520
Reclyptor <5952751+Reclyptor@users.noreply.github.com>
2026-09-05 16:42:17 -0500

    Fixed the MongoDB Shell Alias

--- modules/home/bash/10-aliases.nix
+++ modules/home/bash/10-aliases.nix
@@ -15,7 +15,10 @@ _: {
       # own documented alternative to the flag.
       mysql-reclyptor = "mysql -u$USER -p -h \${GCP_MYSQL_HOST} --ssl-ca=\${GCP_MYSQL_CA_CERT} --ssl-cert=\${GCP_MYSQL_CLIENT_CERT} --ssl-key=\${GCP_MYSQL_CLIENT_KEY}";
       mysql-k3s = "MYSQL_PWD=\${K3S_MYSQL_PASSWORD} mysql -u$USER -h \${K3S_HOST}";
-      mongo-reclyptor = "mongosh '\${ATLAS_MONGODB_HOST}' --apiVersion 1 --username $USER";
+      # Double quotes, not single: the alias body is stored verbatim, so single
+      # quotes would survive into the expansion and hand mongosh the literal
+      # ${ATLAS_MONGODB_HOST}. The URI still needs quoting for its ? and &.
+      mongo-reclyptor = "mongosh \"\${ATLAS_MONGODB_HOST}\" --apiVersion 1 --username $USER";
       # mongosh has no password environment variable, so this one still passes it
       # in argv. Dropping the value from --password would make it prompt instead.
       mongo-k3s = "mongosh --host \${K3S_HOST} --username $USER --password \${K3S_MONGODB_PASSWORD}";
