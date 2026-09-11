b1cfd58f79d81fcffbb8d43a681166454e9cfc81
Reclyptor <5952751+Reclyptor@users.noreply.github.com>
2026-09-11 17:24:38 -0500

    Capped the ZFS ARC at 16 GB on the Workstation

--- modules/workstation/boot.nix
+++ modules/workstation/boot.nix
@@ -9,6 +9,11 @@ _: {
     boot.tmp.cleanOnBoot = true;
     boot.supportedFilesystems.zfs = true;
     boot.zfs.forceImportRoot = false;
+    # The ARC defaults to all but a gigabyte of RAM and gives it back slowly,
+    # so on a 62 GB workstation it sat at 25 GB while 10 GB of the desktop
+    # had been pushed into zram. 16 GB is plenty of read cache for three
+    # pools whose hot set is the nix store and the home directory.
+    boot.kernelParams = [ "zfs.zfs_arc_max=17179869184" ];
     boot.zfs.extraPools = [
       "npool"
       "spool"
